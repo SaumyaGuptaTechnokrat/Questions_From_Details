603acece7a7d16f088db35145ffb7d30d1727773
sgy4765 <65768914+sgy4765@users.noreply.github.com>
2021-11-27 14:24:53 +0530

    Update Questions_2.py

--- Questions_2.py
+++ Questions_2.py
@@ -1,17 +1,21 @@
 import streamlit as st
 import pandas as pd
 
-st.title("REGISTRATION PAGE")
+st.title("QUESTION PAGE")
 
-
+st.header("Scroll Down to Submit Answer")
 
 name = st.text_input("Enter Your Name:")
 id_1 = st.number_input("Enter Phone Number: ")    
 button_1 = st.button("Submit")
 if button_1:
     st.header( f'Hello!! {name}.So Your Question is as below:- ')
-df = pd.read_csv("Registration_Que - Sheet1.csv")
+df = pd.read_csv("C_4.csv")
 
-for i in range(0,4):
-    if name==df.iloc[i].Name and id_1==df.iloc[i].Unique_Id:
-        st.header(df.iloc[i].Question)
+for i in range(0,34):
+    if name==df.iloc[i].NAME_OF_PARTICIPANTS and id_1==df.iloc[i].PHONE_NO:
+        st.header(df.iloc[i].QUESTIONS)
+st.header('*Compulsory Submissions:- ')
+st.header("Circuit, Explain the final Output of the circuit, Video Explanation (if Possible), Uses of Circuit")
+st.header("*Submit Your Answers Here:- ")
+st.header("https://forms.gle/RhiZNk7u8nqnXL999")
